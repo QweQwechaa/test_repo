f68d956b2854bd238986ce5991e903a9577cd9a8
QweQwechaa <qwechy@mail.ru>
2020-09-21 20:36:28 +0300

    init

---
+++ Pervii1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pervii1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string neme = "Олег";
+            int age = 25;
+            Double Weight = 80.5;
+            String stringToShow = neme + ", возраст " + age + ", вес " + Weight;
+            Console.WriteLine(stringToShow);
+        
+        }
+    }
+}
+
